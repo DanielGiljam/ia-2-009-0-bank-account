a67113005e3bdffa91ad43928c6fd8fabdc796ee
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2020-01-09 15:19:36 +0200

    Initializes project and adds base files
    Base files = Transactions.java + Account.java

---
+++ src/com/danielgiljam/ia_2_009_0_bank_account/Transactions.java
@@ -0,0 +1,30 @@
+package com.danielgiljam.ia_2_009_0_bank_account;
+
+//********************************************************************
+//  Transactions.java       Author: Lewis/Loftus
+//
+//  Demonstrates the creation and use of multiple Account objects.
+//********************************************************************
+
+public class Transactions
+{
+   //-----------------------------------------------------------------
+   //  Creates some bank accounts and requests various services.
+   //-----------------------------------------------------------------
+    public static void main(String[] args)
+    {
+        Account acct1 = new Account ("Ted Murphy", 72354, 102.56);
+        Account acct2 = new Account ("Jane Smith", 69713, 40.00);
+        Account acct3 = new Account ("Edward Demsey", 93757, 759.32);
+
+        acct1.deposit (25.85);
+
+        double smithBalance = acct2.deposit (500.00);
+        System.out.println ("Smith balance after deposit: " +
+                smithBalance);
+
+        System.out.println ("Smith balance after withdrawal: " +
+                acct2.withdraw (430.75));
+
+    }
+}
